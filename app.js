0548bf1fab8d7703d69b221c3520448865c932ff
TetianaMalva <malva777@ukr.net>
2019-03-26 13:06:07 +0100

    Get and populate items

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function(){
 
     // Public methods
     return {
+        getItems: function(){
+            return data.items;
+        },
         logData: function(){
             return data;
         }
@@ -30,12 +33,29 @@ const ItemCtrl = (function(){
 
 // UI Controller
 const UICtrl = (function(){
+    const UISelectors = {
+        itemList: '#item-list'
+    }
 
     // Public methods
     return {
+        populateItemList: function(items){
+            let html = '';
 
+            items.forEach(function(item){
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                  <i class="edit item fa fa-tree"></i>
+                  <i class="edit item fa fa-pencil"></i>
+                </a>
+              </li>`;
+            });
+            // Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+
+        }
     }
-
 })();
 
 
@@ -46,7 +66,14 @@ const App = (function(ItemCtrl, UICtrl){
     // Public methods
     return {
         init: function(){
-            console.log('Initializing App...');
+
+            // Fetch items from data structure
+            const items = ItemCtrl.getItems();
+            
+
+            // Populate list with items
+            UICtrl.populateItemList(items);
+
         }
     }
 
